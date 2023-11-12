880a0ddf969cdfd126b4debc909505f6298d3409
Wyatt VO <votranquy96@gmail.com>
2023-11-12 22:54:17 +0700

    Click to zoom image

--- LAB3/js/main.js
+++ LAB3/js/main.js
@@ -4,3 +4,14 @@ function move(direction){
   if(distance < 0 ) distance = 0;
   element.scrollLeft(distance);
 }
+function show(image){
+  let showImage = $('img.imageShow');
+  var fileName = image.src.replace(/^.*[\\\/]/, '');
+  showImage[0].src = './images/big/' + fileName;
+  let showBoard = $('#showBoard');
+  showBoard.show();
+}
+function hide(){
+  let showBoard = $('#showBoard');
+  showBoard.hide();
+}
